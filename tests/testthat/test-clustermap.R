0138e655159bd389bea0ea0f4699ed56e12abec7
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2023-03-03 02:10:00 +0000

    Add clustermap tests

--- tests/testthat/test-clustermap.R
+++ tests/testthat/test-clustermap.R
@@ -2,5 +2,7 @@ test_that("clustermap works", {
   
     dat <- ThreeWayTest::data_matrix_final
     cm <- clustermap(dat = dat)
-    # testthat::expect_true(cm$)
+    testthat::expect_true(methods::is(cm$plot,"plotly"))
+    testthat::expect_true(methods::is(cm$data$X,"matrix"))
+    testthat::expect_true(methods::is(cm$data$row_side_colors,"data.table"))
 })
